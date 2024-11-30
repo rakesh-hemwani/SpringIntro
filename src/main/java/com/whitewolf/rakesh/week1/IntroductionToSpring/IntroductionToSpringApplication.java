a4f0f4777c721647df8ccdebc8b495adee49673f
Rakesh Hemwani <rakeshhemwani654@gmail.com>
2024-11-30 19:47:53 +0530

    Starting Sprring Intro 1

---
+++ src/main/java/com/whitewolf/rakesh/week1/IntroductionToSpring/IntroductionToSpringApplication.java
@@ -0,0 +1,13 @@
+package com.whitewolf.rakesh.week1.IntroductionToSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whitewolf/rakesh/week1/IntroductionToSpring/IntroductionToSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whitewolf.rakesh.week1.IntroductionToSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
